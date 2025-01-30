c40dd692afacb84a2dfd2af32fac82d957ea490b
T13 <rebin.teimouri@elev.ga.ntig.se>
2025-01-30 13:10:43 +0100

    Created connection to db

--- dbconfig.php
+++ dbconfig.php
@@ -1,1 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "baljeet";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 
